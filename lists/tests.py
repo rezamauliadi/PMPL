847eac0306a0a509e72e3b7a7d533d4b65ccf5f0
Reza Mauliadi <reza.mauliadi2@gmail.com>
2015-09-04 17:06:28 +0700

    Test for home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -13,6 +14,11 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
+		
 		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		self.assertIn(b'<title>Coba</title>', response.content)
+		self.assertIn(b'<body>', response.content)
+		self.assertIn(b'<p>Reza Mauliadi</p>', response.content)
+		self.assertIn(b'<p>1506782404</p>', response.content)
+		self.assertIn(b'</body>', response.content)
+		self.assertTrue(response.content.strip().endswith(b'</html>'))
