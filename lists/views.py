47439b409ca837d12b4177aa72f12d4a2a4ae443
Reza Mauliadi <reza.mauliadi2@gmail.com>
2015-09-17 19:06:25 +0700

    New URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,14 +5,16 @@ from lists.models import Item
 def home_page(request):
 	if request.method == 'POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
-	
+		return redirect('/lists/the-only-list-in-the-world/')	
+	return render(request, 'home.html')
+
+def view_list(request):
 	items = Item.objects.all()
-		    
+	
 	komentar = 'oh tidak'
 	if items.count() == 0:
 		komentar = 'yey, waktunya berlibur'
 	elif items.count() < 5:
 		komentar = 'sibuk tapi santai'
 	
-	return render(request, 'home.html', {'items': items, 'komentarhtml': komentar})
+	return render(request, 'list.html', {'items': items, 'komentarhtml': komentar})
