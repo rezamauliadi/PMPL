82eab85810e3da3ded01cdb128abe4feec179cc4
Reza Mauliadi <reza.mauliadi2@gmail.com>
2015-11-12 18:05:12 +0700

    Add not duplicate constrain

--- lists/models.py
+++ lists/models.py
@@ -9,3 +9,7 @@ class List(models.Model):
 class Item(models.Model):
 	text = models.TextField(default='')
 	list = models.ForeignKey(List, default=None)
+
+	class Meta:
+		ordering = ('id',)
+		unique_together = ('list', 'text')
